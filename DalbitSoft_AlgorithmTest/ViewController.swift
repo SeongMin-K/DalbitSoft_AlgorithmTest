ca6969512136125bb824b8d5eae3d5e1990c5e50
SeongMin-K <eo1tks3@naver.com>
2021-10-11 01:41:45 +0900

    Initial Commit

---
+++ DalbitSoft_AlgorithmTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DalbitSoft_AlgorithmTest
+//
+//  Created by SeongMinK on 2021/10/11.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
